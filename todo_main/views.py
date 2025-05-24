616865287dabb5cd2b38f9005bd0728f5a4c9ebc
Daniel <danielcoellovargas@gmail.com>
2025-05-24 17:52:18 -0500

    Template - view

--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse('Hello, world! This is the home page of the Todo app.')
+    return render(request, 'home.html')
