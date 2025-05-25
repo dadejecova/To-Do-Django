cd79491e0c2322dde1931d4e8aef0bc66772932f
Daniel <danielcoellovargas@gmail.com>
2025-05-24 20:31:22 -0500

    app and models

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo',  # Custom app for managing tasks
 ]
 
 MIDDLEWARE = [
