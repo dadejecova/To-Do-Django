80a0fd2bb8293a3c70659bf62ecef69de1d761bf
Daniel <danielcoellovargas@gmail.com>
2025-05-24 21:03:36 -0500

    ModelAdmin Display

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Task
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_completed', 'created_at')
+    list_filter = ('is_completed',)
+    search_fields = ('title',)
+
+admin.site.register(Task, TaskAdmin)
