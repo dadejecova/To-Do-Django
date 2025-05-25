d8c5e36cfd8ae7db9dd6af9f042b9eed0888199d
Daniel <danielcoellovargas@gmail.com>
2025-05-25 00:06:14 -0500

    Done Button

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect, HttpResponse
 from .models import Task
 
 # Create your views here.
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(title=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
     return redirect('home')
